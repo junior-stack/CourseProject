2a74f82e8f8f3d1e6896ccb899fa25a70b13ba33
Jun Xing <royxj2018@outlook.com>
2020-11-13 18:39:04 -0500

    Added hashcode() and equals() for attendee and organizer

--- phase1/src/Entity/Attendee.java
+++ phase1/src/Entity/Attendee.java
@@ -6,4 +6,17 @@ public class Attendee extends User {
     super(username, password, phone, email);
     this.identity = "Attendee";
   }
+
+
+  @Override
+  public boolean equals(Object other) {
+    if (!(other instanceof Attendee)) {
+      return false;
+    }
+    Attendee o = (Attendee) other;
+    return o.getUserId() == this.getUserId() && o.getPassword().equals(this.getPassword()) && o
+            .getUsername().equals(this.getUsername())
+            && o.getEvents().equals(this.getEvents()) && o.getEmail().equals(this.getEmail()) && o
+            .getPhone().equals(this.getPhone()) && o.getIdentity().equals(this.getIdentity());
+  }
 }
--- phase1/src/Entity/Organizer.java
+++ phase1/src/Entity/Organizer.java
@@ -6,4 +6,16 @@ public class Organizer extends User {
     super(username, password, phone, email);
     this.identity = "Organizer";
   }
+
+  @Override
+  public boolean equals(Object other) {
+    if (!(other instanceof Organizer)) {
+      return false;
+    }
+    Organizer o = (Organizer) other;
+    return o.getUserId() == this.getUserId() && o.getPassword().equals(this.getPassword()) && o
+            .getUsername().equals(this.getUsername())
+            && o.getEvents().equals(this.getEvents()) && o.getEmail().equals(this.getEmail()) && o
+            .getPhone().equals(this.getPhone()) && o.getIdentity().equals(this.getIdentity());
+  }
 }
