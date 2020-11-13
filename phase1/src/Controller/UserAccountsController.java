a27597e70d837d8ac602d4750f1f6dd23336e9dd
Ziwei Jia <jiaziwei1108@gmail.com>
2020-11-13 00:10:36 -0500

    major problems will be solved....

--- phase1/src/Controller/UserAccountsController.java
+++ phase1/src/Controller/UserAccountsController.java
@@ -1,4 +1,13 @@
 package Controller;
 
+import UseCase.UserAccountManager;
+
 public class UserAccountsController {
+  public UserAccountsController(UserAccountManager userAccountManager){this.userAccountManager = userAccountManager;}
+
+  public void createAttendee(String username, String password, String phone, String email){
+    userAccountManager.createAttendee(username, password, phone, email);
+  }
+
+  public boolean verify(String username, String password){return userAccountManager.verifyUser(username,password);}
 }
