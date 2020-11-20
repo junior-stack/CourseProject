692474cbf2c120e02453bf118328d056ac6155c5
Jun Xing <royxj2018@outlook.com>
2020-11-19 22:20:05 -0500

    4th change

--- phase1/src/Exception/DoubleBooking.java
+++ phase1/src/Exception/DoubleBooking.java
@@ -1,4 +1,4 @@
-package exception;
+package Exception;
 
 import Entity.Room;
 import java.sql.Time;
--- phase1/src/Exception/SignupConflict.java
+++ phase1/src/Exception/SignupConflict.java
@@ -1,4 +1,4 @@
-package exception;
+package Exception;
 
 import Entity.Event;
 import Entity.User;
