c998d2d8f476cc1b1bc57b3db93ba6ea51920944
Jiahao Zhang <zjh4568@gmail.com>
2020-11-27 10:56:04 +0800

    Add attributes for Message Entity.

--- phase2/app/src/Files/Entity/Message.java
+++ phase2/app/src/Files/Entity/Message.java
@@ -1,4 +1,12 @@
-package Files.Entity;
+package Entity;
 
 public class Message {
+    private static int counter = 0;
+    private int messageId;
+    private String senderEmail;
+    private String receiverEmail;
+    private String content;
+    private String status;
+
+
 }
