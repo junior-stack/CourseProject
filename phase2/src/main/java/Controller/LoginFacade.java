0e4203276d3014a6eb2a5fac84dff3b9c1ac0190
junior-stack <JaxYezhou@163.com>
2021-08-02 19:50:35 +0800

    configure deploy

---
+++ phase2/src/main/java/Controller/LoginFacade.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import Dao.UserDao;
+import Presenter.LoginPresenter;
+import UseCase.UserAccountManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * This class represents an LoginFacade.
+ *
+ * @author Jun Xing
+ * @version 1.0
+ */
+@Controller
+public class LoginFacade {
+
+  @RequestMapping("/login")
+  public String test(Model model){
+    model.addAttribute("END", "login");
+    return "index";
+  }
+
+}
